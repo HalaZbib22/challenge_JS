940b4ae33598f441c52832b01653aa9c2f1363f9
Hala Zbib <halazbib22@gmail.com>
2022-01-28 02:12:08 +0200

    DONE!

--- solution.js
+++ solution.js
@@ -1,88 +1,113 @@
+window.onload = function () {
+  pattern = [];
+  checker = [];
+  audio = document.createElement("audio");
+  header = document.getElementById("title");
+  $(document).keydown(Play);
 
-  window.onload = function () {
+  function PlayAudio(a) {
+    audio.setAttribute("src", "sounds/" + a + ".mp3");
+    audio.play();
+  }
+
+  function StartOver() {
     pattern = [];
-    user = [];
-    buttons = document.querySelectorAll(".container .btn");
-    console.log("buttons are", buttons);
-    header = document.getElementById("title");
-    Array.from(buttons).forEach(function (btn) {
-      btn.addEventListener("click", Play);
+    checker = [];
+    header.textContent = "Press Any Key to Start";
+    Play();
+  }
+
+  function Play() {
+
+    $(document).off("keydown");
+    var random = Math.floor(Math.random() * 4);
+    pattern += random;
+    checker = [...pattern];
+    console.log("pattern:", pattern, "length = ", pattern.length);
+    header.textContent = "level" + pattern.length;
+
+    LastElement = pattern.slice(-1)[0];
+    console.log("LAST ELEMENT::::",LastElement);
+
+    if (LastElement == 0) {
+      $("#green").fadeOut(210).fadeIn(210);
+      PlayAudio("green");
+    } else if (LastElement == 1) {
+      $("#red").fadeOut(210).fadeIn(210);
+      PlayAudio("red");
+    } else if (LastElement == 2) {
+      $("#yellow").fadeOut(210).fadeIn(210);
+      PlayAudio("yellow");
+    } else if (LastElement == 3) {
+      $("#blue").fadeOut(210).fadeIn(210);
+      PlayAudio("blue");
+    }
+
+    $("#red").click(function () {
+      $("#red").addClass("pressed");
+      setTimeout(function () {
+        $("#red").removeClass("pressed");
+      }, 120);
+      PlayAudio("red");
+      userInput(1);
     });
+    $("#green").click(function () {
+      $("#green").addClass("pressed");
+      setTimeout(function () {
+        $("#green").removeClass("pressed");
+      }, 120);
+      PlayAudio("green");
+      userInput(0);
+    });
+    $("#yellow").click(function () {
+      $("#yellow").addClass("pressed");
+      setTimeout(function () {
+        $("#yellow").removeClass("pressed");
+      }, 120);
+      PlayAudio("yellow");
+      userInput(2);
+    });
+    $("#blue").click(function () {
+      $("#blue").addClass("pressed");
+      setTimeout(function () {
+        $("#blue").removeClass("pressed");
+      }, 120);
+      PlayAudio("blue");
+      userInput(3);
+    });
+  }
+  function userInput(i) {
+    console.log(checker);
 
-    function startOver() {
-      header.innerHTML = "Press Any Key to Start";
-      Array.from(buttons).forEach(function (btn) {
-        btn.removeEventListener("click", startOver);
-      });
-      pattern = [];
-      user = [];
-      var random = Math.floor(Math.random() * 4);
-      console.log("random:", random);
-      // pattern += random;
-      // console.log("pattern:",pattern);
-      console.log("buttons:", buttons);
-      console.log("buttons element:", buttons[random]);
-      pattern += random;
-      console.log("pattern:", pattern, "length = ", pattern.length);
-      Array.from(buttons).forEach(function (btn) {
-        btn.addEventListener("click", Play);
-      });
+    if (checker[0] != i) {
+      return End();
+    } else if (checker[0] == i) {
+      console.log(checker);
+      checker.shift();
+      console.log(checker);
     }
 
-    function Play() {
-        user = [];
-        console.log("STARTING USER::",user, "AND STARTING LENGTH", user.length)
-      Array.from(buttons).forEach(function (btn) {
-        btn.removeEventListener("click", Play);
-      });
-
-      var random = Math.floor(Math.random() * 4);
-      console.log(random);
-      // pattern += random;
-      // console.log("pattern:",pattern);
-    //   console.log("buttons:", buttons);
-    //   console.log("buttons element:", buttons[random]);
-      pattern += random;
-      console.log("pattern:", pattern);
-
-      header.innerHTML = "level" + pattern.length;
-
-      Array.from(buttons).forEach(function (btn) {
-        btn.addEventListener("click",Userinput)});
-      
+    if (checker.length == 0) {
+      return setTimeout(function () {
+        $("#green").off();
+        $("#red").off();
+        $("#yellow").off();
+        $("#blue").off();
+        Play();
+      }, 1000);
     }
-      function Userinput(){
-        Array.from(buttons).forEach(function (btn) {
-            btn.removeEventListener("click",Userinput)});
-            userChoice = buttons.onClick.getElement;
-            index = Array.from(buttons).indexOf(userChoice);
-            console.log("USER BEFORE APPENDING", user);
-            user += index;
-            console.log("USER :", user,"USER LENGTH :", user.length);
+  }
+  function End() {
+    console.log("Game OVER!");
+    setTimeout(PlayAudio("wrong"), 210);
 
-            if (pattern.length === user.length) {
-                if (pattern === user){
-                console.log("pattern is = ", pattern, "while user is = ", user);
-                Play();
-            }else{
-               console.log("YOU LOST"); }
-              } else {
-                console.log("CONTINUE!!!!")
-                Array.from(buttons).forEach(function (btn) {
-                    btn.addEventListener("click",Userinput )});
-                Userinput();
-              }
+    $("#title").text("Game over, Press Any Key to Restart");
 
-        
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 210);
 
-    }
-      
-
-    // var getid = divparent[random].getAttribute('id')
-    // var audio = new Audio('red.mp3');
-
-    // console.log("child element:", btns);
-
-    // console.log('parent element to remove child from:', li.parentElement);
-    // li.parentNode.removeChild(li);
-  };
+    $(document).keydown(StartOver);
+  }
+};
